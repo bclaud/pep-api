9d56de17b7594f93790c36c4422a9c595af99aa9
bclaud <bruarrais@gmail.com>
2022-02-03 21:31:19 -0300

    httpoison added

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Pep.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:httpoison, "~> 1.8"}
     ]
   end
 
