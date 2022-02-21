278ef35e86ccb19cda4762f69327dad3fb9e8d0e
bclaud <bruarrais@gmail.com>
2022-02-21 18:48:48 -0300

    atualizado versao

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Pep.MixProject do
   def project do
     [
       app: :pep,
-      version: "0.1.0",
+      version: "0.9.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:gettext] ++ Mix.compilers(),
