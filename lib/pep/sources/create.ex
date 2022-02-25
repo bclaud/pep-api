5c1e923f6a67ed25699404df3a5522c9c18ccd7d
bclaud <bruarrais@gmail.com>
2022-02-25 16:36:37 -0300

    parse adicionado as funcoes de importacao da fonte

--- lib/pep/sources/create.ex
+++ lib/pep/sources/create.ex
@@ -1,8 +1,9 @@
 defmodule Pep.Sources.Create do
+  NimbleCSV.define(CSVParser, separator: ";", escape: "\"")
+
   alias Pep.{Source, Repo, Error}
 
   alias Pep.Pep, as: PepStruct
-  alias Pep.Parser
   alias Ecto.Changeset
 
   def call(ano_mes) when is_binary(ano_mes) do
@@ -79,8 +80,61 @@ defmodule Pep.Sources.Create do
   defp parse_import_to_db(ano_mes) do
     source = Repo.get_by(Source, ano_mes: ano_mes)
 
-    Parser.call(source)
+    parse(source)
     |> Enum.map(fn pep -> PepStruct.changeset(pep) end)
     |> Enum.each(fn changeset -> Pep.Repo.insert(changeset) end)
+  end
+
+  defp parse(%{ano_mes: ano_mes, id: id} = _source) do
+    ("priv/reports/" <> ano_mes <> "_PEP.csv")
+    |> File.stream!()
+    |> CSVParser.parse_stream()
+    |> Stream.map(fn [
+                       cpf,
+                       nome,
+                       sigla,
+                       descr,
+                       nivel,
+                       regiao,
+                       data_inicio,
+                       data_fim,
+                       data_carincia
+                     ] ->
+      %{
+        cpf: :binary.copy(cpf),
+        nome: :binary.copy(nome),
+        sigla: :binary.copy(sigla),
+        descr: :binary.copy(descr),
+        nivel: :binary.copy(nivel),
+        regiao: :binary.copy(regiao),
+        data_inicio: :binary.copy(data_inicio),
+        data_fim: :binary.copy(data_fim),
+        data_carincia: :binary.copy(data_carincia),
+        source_id: ""
+      }
+    end)
+    |> Enum.to_list()
+    |> Enum.map(fn pep -> fix_enconding(pep) end)
+    |> Enum.map(fn pep -> %{pep | source_id: id} end)
+    |> Enum.map(fn %{cpf: cpf} = pep -> %{pep | cpf: sanitize_cpf(cpf)} end)
+  end
+
+  defp fix_enconding(pep) do
+    pep
+    |> Enum.map(fn {key, value} -> {key, latin1_to_utf8(value)} end)
+    |> Enum.into(%{})
+  end
+
+  defp latin1_to_utf8(binary),
+    do:
+      :unicode.characters_to_binary(
+        binary,
+        :latin1,
+        :utf8
+      )
+
+  defp sanitize_cpf(cpf) do
+    cpf
+    |> String.replace(["*", ".", "-"], "")
   end
 end
