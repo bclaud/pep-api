34375a1fdb9a88476893b38175876b374ba2b4f1
Bruno Claudino <bruarrais@gmail.com>
2022-10-12 12:32:06 -0300

    update elixir ver

--- flake.nix
+++ flake.nix
@@ -1,55 +1,36 @@
 {
-  description = "A very basic flake";
+    description = "Development environment";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.05";
-
-  outputs = { self, nixpkgs }: {
-
-
-    devShells.x86_64-linux.default =
-      let
-        pkgs = import nixpkgs { system = "x86_64-linux"; };
-      in
-      pkgs.mkShell {
-        name = "gosto";
-        buildInputs =
-          with pkgs; [
-            gnumake
-            gcc
-            readline
-            openssl
-            zlib
-            libxml2
-            curl
-            libiconv
-            glibcLocales
-            elixir
-            erlang
-          ];
-        shellHook = ''
-          # create local tmp folders
-          mkdir -p .nix-mix
-          mkdir -p .nix-hex
-
-          mix local.hex --force --if-missing
-          mix local.rebar --force --if-missing
-
-          # to not conflict with your host elixir
-          # version and supress warnings about standard
-          # libraries
-          export ERL_LIBS="$HEX_HOME/lib/erlang/lib"
-
-          export GPG_TTY="$(tty)"
-
-          # this allows mix to work on the local directory
-          export MIX_HOME=$PWD/.nix-mix
-          export HEX_HOME=$PWD/.nix-mix
-          export PATH=$MIX_HOME/bin:$HEX_HOME/bin:$PATH
-          export ERL_AFLAGS="-kernel shell_history enabled"
-
-          export LANG=en_US.UTF-8
-        '';
-      };
-
+  inputs = {
+      nixpkgs = { url = "github:NixOS/nixpkgs/nixpkgs-unstable"; };
+      flake-utils = { url = "github:numtide/flake-utils"; };
   };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        inherit (nixpkgs.lib) optional;
+        pkgs = import nixpkgs { inherit system; };
+        beamPkg = pkgs.beam.packagesWith pkgs.erlangR25;
+
+        elixir = beamPkg.elixir.override {
+            version = "1.14.1";
+            sha256 =  "/QQckiRvwmD3gdIo19TXM0bIgdxNx8eQwpd1RnEo35A=";
+        };
+
+        elixir-ls = pkgs.beam.packages.erlang.elixir_ls;
+        locales = pkgs.glibcLocales;
+      in
+      with pkgs;
+      {
+          devShell = pkgs.mkShell
+          {
+              buildInputs = [
+                erlangR25
+                elixir
+                locales
+            ];
+          };
+      }
+    );
 }
