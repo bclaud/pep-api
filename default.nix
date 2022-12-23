0b0c44c5141165b7fe539f9cd8c90972a44a293d
Bruno Claudino <bruarrais@gmail.com>
2022-12-23 18:20:23 -0300

    update default flake nix

--- default.nix
+++ default.nix
@@ -3,16 +3,10 @@
 with pkgs;
 
 let
-  pname = "pep-api";
+  pname = "pep";
   version = "0.0.1";
   src = ./.;
 
-  mixFodDeps = beamPackages.fetchMixDeps {
-    pname = "${pname}-deps";
-    inherit src version;
-    sha256 = lib.fakeSha;
-  };
-
   mixNixDeps = with pkgs; import ./mix_deps.nix { inherit lib beamPackages; };
 in
-  beamPackages.mixRelease { inherit mixNixDeps;}
+  beamPackages.mixRelease { inherit mixNixDeps pname version src; }
