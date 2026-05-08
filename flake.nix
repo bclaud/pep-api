e8d440ae622788d12a669d514b3bd1f7840a13b9
Bruno Claudino <bruarrais@gmail.com>
2026-05-07 22:02:33 -0300

    build: use Garnix action to push container to GHCR
    Garnix actions (not the deploy key) are the correct way to push
    container images. Define an x86_64-linux app that decrypts the
    GHCR token via age and uses skopeo to push the aarch64 container
    to ghcr.io.

--- flake.nix
+++ flake.nix
@@ -12,57 +12,82 @@
         "x86_64-linux"
         "aarch64-linux"
       ];
-    in
-    flake-utils.lib.eachSystem supportedSystems (system:
-      let
-        beamPkgs = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang_28;
-        beamPackages = beamPkgs // {
-          elixir = beamPkgs.elixir_1_19;
-          hex = beamPkgs.hex.override { elixir = beamPkgs.elixir_1_19; };
-          rebar3 = beamPkgs.rebar3;
-        };
 
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            (self: super: {
-              inherit beamPackages;
-            })
-          ];
-        };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          name = "pep-api";
-
-          packages = with pkgs; [
-            elixir_1_19
-            elixir-ls
-            postgresql
-            mix2nix
-            inotify-tools
-            glibcLocalesUtf8
-          ];
-
-          env = {
-            MIX_HOME     = "${placeholder "out"}/.mix";
-            HEX_HOME     = "${placeholder "out"}/.hex";
-            LANG         = "en_US.UTF-8";
-            LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else "";
+      perSystem = flake-utils.lib.eachSystem supportedSystems (system:
+        let
+          beamPkgs = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang_28;
+          beamPackages = beamPkgs // {
+            elixir = beamPkgs.elixir_1_19;
+            hex = beamPkgs.hex.override { elixir = beamPkgs.elixir_1_19; };
+            rebar3 = beamPkgs.rebar3;
           };
 
-          shellHook = ''
-            echo "pep-api dev shell (Elixir 1.19 / OTP 28)"
-          '';
-        };
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [
+              (self: super: {
+                inherit beamPackages;
+              })
+            ];
+          };
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            name = "pep-api";
 
-        packages = rec {
-          pep = pkgs.callPackage ./default.nix { };
-          container = pkgs.callPackage ./container.nix { inherit pep; };
-          defaultPackage = pep;
-        };
+            packages = with pkgs; [
+              elixir_1_19
+              elixir-ls
+              postgresql
+              mix2nix
+              inotify-tools
+              glibcLocalesUtf8
+            ];
 
-        formatter = pkgs.nixpkgs-fmt;
-      }
-    );
+            env = {
+              MIX_HOME     = "${placeholder "out"}/.mix";
+              HEX_HOME     = "${placeholder "out"}/.hex";
+              LANG         = "en_US.UTF-8";
+              LOCALE_ARCHIVE = if pkgs.stdenv.isLinux then "${pkgs.glibcLocalesUtf8}/lib/locale/locale-archive" else "";
+            };
+
+            shellHook = ''
+              echo "pep-api dev shell (Elixir 1.19 / OTP 28)"
+            '';
+          };
+
+          packages = rec {
+            pep = pkgs.callPackage ./default.nix { };
+            container = pkgs.callPackage ./container.nix { inherit pep; };
+            defaultPackage = pep;
+          };
+
+          formatter = pkgs.nixpkgs-fmt;
+        }
+      );
+
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+
+      pushContainer = pkgs.writeShellApplication {
+        name = "push-container";
+        runtimeInputs = [ pkgs.age pkgs.skopeo ];
+        text = ''
+          set -euo pipefail
+          export HOME=$PWD
+          TOKEN=$(age -d -i "$GARNIX_ACTION_PRIVATE_KEY_FILE" "${
+            ./secrets/ghcr-token.age
+          }")
+          skopeo copy \
+            --dest-creds "bclaud:$TOKEN" \
+            "docker-archive:${self.packages.aarch64-linux.container}" \
+            docker://ghcr.io/bclaud/pep-api/pep-container:latest
+        '';
+      };
+    in
+    perSystem // {
+      apps.x86_64-linux.push-container = {
+        type = "app";
+        program = "${pushContainer}/bin/push-container";
+      };
+    };
 }
